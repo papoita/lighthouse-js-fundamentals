355c9b525f2c4d2d8a4eab1e1dd90ac5bf6ad5bf
PaolaP <paolaperez8@gmail.com>
2021-09-20 03:35:18 +0000

    code challenge conditionals

---
+++ which-school.js
@@ -0,0 +1,16 @@
+/** @format */
+const whichSchool = function whichSchool(age) {
+	if (age < 13) {
+		return `Elementary School`;
+	} else if (age > 18) {
+		return `Lighthouse Labs`;
+	} else {
+		return `Secondary School`;
+	}
+};
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
