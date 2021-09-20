0d994b1290e249f4b752a9d5a60dc473543b806b
PaolaP <paolaperez8@gmail.com>
2021-09-20 03:07:36 +0000

    add more conditionals examples

--- conditionals.js
+++ conditionals.js
@@ -32,3 +32,11 @@ const age = 26;
 if (isCitizen && age > 18) {
 	console.log("You are eligible to vote.");
 }
+
+if (temperature < -40 || temperature > 40) {
+	console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+	console.log("Leave your umbrella at home!");
+}
