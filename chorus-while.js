81085631a7eb9d2fdb705dbec7a924611d978f08
PaolaP <paolaperez8@gmail.com>
2021-09-21 00:38:39 +0000

    excercise of while and for loops

---
+++ chorus-while.js
@@ -0,0 +1,21 @@
+/*const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 10) {
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
+*/
+
+//example 2 to become less repetitive in chorus
+
+const chorus2 = "Let's dance!";
+let repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus2);
+  repeat++;
+}
+console.log("Until the sun comes up!");
