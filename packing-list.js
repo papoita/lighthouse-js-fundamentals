60964dde06d32d44abf6081be519d406528a0d29
PaolaP <paolaperez8@gmail.com>
2021-09-21 03:10:14 +0000

    iterating over array with while

--- packing-list.js
+++ packing-list.js
@@ -1,8 +1,19 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+/* iteraring over array with for
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 packingList[3];
 //'pans'
+
+*/
+
+//Iterating over array with while
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
