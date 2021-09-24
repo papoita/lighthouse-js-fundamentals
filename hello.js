1cbb3ac196ffe4fc40fdc94d0f79b7f4f50e9abb
PaolaP <paolaperez8@gmail.com>
2021-09-24 23:41:55 +0000

    hello example functions

---
+++ hello.js
@@ -0,0 +1,14 @@
+const sayHello = function () {
+  console.log("Hello, world");
+}
+
+sayHello(3);
+
+//other example
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
