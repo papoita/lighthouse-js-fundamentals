26a9cc04f8a265e30e06936916f45b38e4637567
PaolaP <paolaperez8@gmail.com>
2021-09-24 21:27:18 +0000

    result vs console.log

--- function_tinkering.js
+++ function_tinkering.js
@@ -16,3 +16,35 @@ function sayHello() {
   return message; // returns value instead of printing it
 }
 console.log(sayHello());
+
+//A parameter is always going to be a variable name and appears in the function declaration. On the other hand, an argument is always going to be a value (i.e. any of the JavaScript data types - a number, a string, a boolean, etc.) and will always appear in the code when the function is called or invoked.
+
+//Write a function called laugh() that takes one parameter, num that represents the number of "ha"s to return
+var sound = "";
+function laugh(num) {
+  for (var x = 0; x < num; x++) {
+    sound = sound + "ha";
+  }
+  sound = sound + "!";
+  return sound;
+}
+
+console.log(laugh(3));
+
+//return is very diff to console.log
+
+// example of using the retrun output to be used into another function
+// returns the sum of two numbers
+function add(x, y) {
+  return x + y;
+}
+
+
+// returns the value of a number divided by 2
+function divideByTwo(num) {
+  return num / 2;
+}
+
+
+var sum = add(5, 7); // call the "add" function and store the returned value in the "sum" variable
+var average = divideByTwo(sum); // call the "divideByTwo" function and store the returned value in the "average" variable
