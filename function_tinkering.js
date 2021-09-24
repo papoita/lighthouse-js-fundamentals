705a7923bd0d774e0ea2043ed92f6c38c0c15ad3
PaolaP <paolaperez8@gmail.com>
2021-09-24 21:47:48 +0000

    build triangle using function

--- function_tinkering.js
+++ function_tinkering.js
@@ -59,3 +59,31 @@ var average = divideByTwo(sum); // call the "divideByTwo" function and store the
 //then variables
 //after functions can be called
 
+//function that builds a line
+function makeLine(length) {
+  var line = "";
+  for (var j = 1; j <= length; j++) {
+    line += "* "
+  }
+  return line + "\n";
+}
+// your code goes here.  Make sure you call makeLine() in your own code.
+function buildTriangle(length) {
+  // Let's build a huge string equivalent to the triangle
+  var triangle = "";
+
+  //Let's start from the topmost line
+  var lineNumber = 1;
+
+  for (lineNumber = 1; lineNumber <= length; lineNumber++) {
+    // We will not print one line at a time.
+    // Rather, we will make a huge string that will comprise the whole triangle
+    triangle = triangle + makeLine(lineNumber);
+  }
+  return triangle;
+}
+
+// test your code by uncommenting the following line
+// Note that the function buildTriangle() must return a string
+// because the console.log() accepts a string argument
+console.log(buildTriangle(10));
