c793ab573b00559686b8d4430359bd21e01110aa
PaolaP <paolaperez8@gmail.com>
2021-09-26 12:00:00 +0100

    t pull
    git pull
    testing x and y
    
    "testing date stamp isse"
    git commit -m "testing date stamp issue"

--- looping-lastIndexOf.js
+++ looping-lastIndexOf.js
@@ -1,6 +1,6 @@
 let moves = ['north', 'north', 'west', 'west', 'north', 'east', 'north']
 //let moves = ["north", "west"]
-
+/*
 function finalPosition(moves) {
   let paradePosition = [];
   for (moves of moves);
@@ -15,3 +15,50 @@ function finalPosition(moves) {
   return finalPosition();
 }
 finalPosition(moves);
+*/
+/*
+const moves = ['north', 'north', 'west', 'west', 'north', 'east', 'north'];
+
+const finalPosition = function (moves) {
+  let paradePosition = [];
+  let x = 0;
+  let y = 0;
+  for (const move of moves) {
+    if (moves === 'north') {
+      y = y + 1;
+    } else if (moves === 'south') {
+      y = y - 1;
+    } else if (moves === 'west') {
+      x = x + 1;
+    } else if (moves === 'east') {
+      x = x - 1;
+    }
+  }
+  paradePosition.push(x, y);
+  return paradePosition;
+}
+
+console.log(finalPosition(moves));
+*/
+//
+
+function finalPosition(moves) {
+  let paradePosition = [0, 0];
+  for (moves of move) {
+    if (paradePosition === "north") {
+      paradePosition[1] = 1;
+    }
+    else if (paradePosition === "south") {
+      paradePosition[1] = -1;
+    }
+    else if (paradePosition === "east") {
+      paradePosition[0] = 1;
+    }
+    else if (paradePosition === "west") {
+      paradePosition[0] = -1;
+    }
+  }
+  return paradePosition;
+}
+
+console.log(finalPosition(moves));
