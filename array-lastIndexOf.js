f57cc698a18ee07b6fc4dc5f1d26875c736e2984
PaolaP <paolaperez8@gmail.com>
2021-09-27 23:20:54 +0000

    first code for soluiton

---
+++ array-lastIndexOf.js
@@ -0,0 +1,10 @@
+function lastIndexOf() {
+
+
+}
+
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
