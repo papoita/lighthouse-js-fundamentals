d7ef794b47b406482d5a6c3e28a1dfb7fa262fc9
PaolaP <paolaperez8@gmail.com>
2021-09-27 00:59:29 +0000

    testing if and else if

--- looping-lastIndexOf.js
+++ looping-lastIndexOf.js
@@ -1,6 +1,15 @@
 const moves = ['north', 'north', 'west', 'west', 'north', 'east', 'north']
 
-function finalPosition() {
+function finalPosition(moves) {
+  let paradePosition = [];
+  if (moves = "north") {
+    paradePosition = paradePosition.push[0](1);
+
+  } if (moves = "east") {
+    paradePosition = paradePosition.push[1](1);
+  } else if (moves = "west") {
+    paradePosition = paradePosition.push[1](-1);
+  }
 
 }
 finalPosition(moves);
