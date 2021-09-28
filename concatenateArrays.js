6212c70884d3dc5eb06d7949b2764090b002a514
PaolaP <paolaperez8@gmail.com>
2021-09-28 02:12:39 +0000

    accepted code

---
+++ concatenateArrays.js
@@ -0,0 +1,10 @@
+function concat(array1, array2) {
+  let array1 = [];
+  let array2 = [];
+  let array3 = array1.concat(array2);
+  return array3;
+  console.log(array3);
+}
+
+
+concat([1, 2, 3], [4, 5, 6]);
