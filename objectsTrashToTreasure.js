af22864f82ae6b4106bece38428041cc3bc872b7
PaolaP <paolaperez8@gmail.com>
2021-09-28 03:17:49 +0000

    testing diff codes

--- objectsTrashToTreasure.js
+++ objectsTrashToTreasure.js
@@ -1,22 +1,20 @@
 function smartGarbage(trash, bins) {
-  let type = "";
-  let x = 0;
-  let y = 0;
-  let z = 0;
-  let bins = {
-    waste: x,
-    recycling: y,
-    compost: z,
+  if (type = "waste") {
+    return bins.waste + 1;
+  } if (type = "recycling") {
+    return bins.recycling + 1;
+  } if (type = "compost") {
+    return bins.compost + 1;
   }
-  for (garbage in bins) {
-    if (type = "waste") {
-      let bins = bins.waste(x + 1);
-    } if (type = "recycling") {
-      let bins = bins.recycling(y + 1);
-    } if (type = "compost") {
-      let bins = bins.compost(z + 1);
-    }
-  } return bins;
 }
 smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 });
 
+// let type = "";
+let x = 0;
+let y = 0;
+let z = 0;
+let bins = {
+  waste: x,
+  recycling: y,
+  compost: z
+}
