64aa14b3e971b86f8c96c1811b2d170ec3d6cb82
PaolaP <paolaperez8@gmail.com>
2021-10-04 19:32:56 +0000

    code success

--- prep-work/kataThreeVowels.js
+++ prep-work/kataThreeVowels.js
@@ -1,23 +1,19 @@
 //In this exercise, we will be counting the number of vowels that appear in a given string.For this exercise, consider the following to be vowels: a, e, i, o, and u.
 
-function numberOfVowels(data) {
-  let vowels = ["y"];
-  for (let i = 0; i < data.length; i++) {
-    if (i === "a" || i === "e" || i === "i" || i === "o" || i === "u") {
-      vowels.push(i);
-      vowels.length;
-    }
-  }
 
-  // Put your solution here
-}
 
 function numberOfVowels(data) {
   console.log(data);
-
+  let vowels = [];
   for (let i = 0; i < data.length; i++) {
-    console.log(data[i]);
-  }
+    //console.log(data[i]);
+
+    if (data[i] === "a" || data[i] === "e" || data[i] === "i" || data[i] === "o" || data[i] === "u") {
+      //console.log(data[i]);
+      //vowelArray = vowelArray.push(data[i]);
+      vowels += data[i];
+    }
+  } return vowels.length;
 }
 
 
@@ -29,3 +25,16 @@ console.log(numberOfVowels("aeiou")); //5
 //and add everytime it encounters  conditions === a,e,i,o,u.
 //create a new array
 //count length of array
+
+//Other tests
+function numberOfVowels(data) {
+  let vowels = ["y"];
+  for (let i = 0; i < data.length; i++) {
+    if (i === "a" || i === "e" || i === "i" || i === "o" || i === "u") {
+      vowels.push(i);
+      vowels.length;
+    }
+  }
+
+  // Put your solution here
+}
