e0a43888a718747e6e22e78e0102b4989fed2971
PaolaP <paolaperez8@gmail.com>
2021-10-06 01:11:44 +0000

    successcode

--- prep-work/kataFivePercentEcondedString.js
+++ prep-work/kataFivePercentEcondedString.js
@@ -10,10 +10,40 @@
 //loop again new string for last character to check everycharacter in the string
 //if string.length -1 = %20 remove it
 
+//removing any space encodement after string
+//adding an if statement that checks itext[i].length - 1 || text[i].length + 1 are non space chars and arent over or under the length size
+
+//https://gist.github.com/papoita/a3c0d084ee61f357c30e4a557a6df0bb
+
+function urlEncode(text) {
+  let encodedText = [];
+  text = text.trim();
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] == " ") {
+      encodedText = encodedText + "20%";
+      //console.log("Am i doing this loop");
+
+    } else {
+      encodedText += text[i];
+      //console.log("Am i doing this second loop");
+    }
+
+  } console.log(encodedText);
+}
+
+
+
+
+
+
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure"));
 
 //replace and add to new string
 function urlEncode(text) {
   let encodedText = [];
+
   for (let i = 0; i < text.length; i++) {
     if (text[i] == " ") {
       encodedText.push("20%");
@@ -28,9 +58,7 @@ function urlEncode(text) {
   }
 }
 
-console.log(urlEncode("Lighthouse Labs"));
-console.log(urlEncode(" Lighthouse Labs "));
-console.log(urlEncode("blue is greener than purple for sure"));
+
 
 
 
